9413b50c6d31f64d3d45c8b16c3d9ea66eaf651b
Trevor Mathiak <tmathiak@gmail.com>
2023-09-26 21:07:47 -0400

    Add nav config and types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,35 @@
-import { OpenGraphType } from "next/dist/lib/metadata/types/opengraph-types";
+import { OpenGraphType } from "next/dist/lib/metadata/types/opengraph-types"
+
+interface NavItem {
+  title: string
+  href: string
+}
+
+export interface NavConfig {
+  navItems: NavItem[]
+}
 
 export interface SiteConfig {
-  title: string;
-  description: string;
-  author: string[];
-  website: string;
-  keywords: string[];
-  url: string;
+  title: string
+  description: string
+  author: string[]
+  website: string
+  keywords: string[]
+  url: string
   openGraph: {
-    title: string;
-    description: string;
-    url: string;
-    site_name: string;
-    images: string;
-    type: OpenGraphType;
-    locale: string;
-  };
+    title: string
+    description: string
+    url: string
+    site_name: string
+    images: string
+    type: OpenGraphType
+    locale: string
+  }
   icons: {
-    icon: string;
-    shortcut: string;
-    apple: string;
-  };
-  links: string;
-  manifest: string;
+    icon: string
+    shortcut: string
+    apple: string
+  }
+  links: string
+  manifest: string
 }
