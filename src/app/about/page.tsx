a56d8e4fd91e89745e048e83ea8c6ce72da6a7e8
Trevor Mathiak <tmathiak@gmail.com>
2023-09-29 12:39:20 -0400

    Set up about, contact, and products pages
    Wanted to avoid 404 errors when clicking the links

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <h1 className="font-medium font-heading text-6xl">About Page</h1>
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductsPage() {
+  return <h1 className="font-medium font-heading text-6xl">Products Page</h1>
+}
