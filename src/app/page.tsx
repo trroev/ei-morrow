cce8de4a0f3f8684db216aa61788aa8e7a80381e
Trevor Mathiak <tmathiak@gmail.com>
2023-09-29 12:52:49 -0400

    Edit index page for responsiveness testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 export default function IndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-medium font-heading text-6xl">
-        Hello, E. I. Morrow Co., Inc.!
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla fuga,
-        deserunt obcaecati ab sunt voluptas culpa necessitatibus consequatur
-        ipsa ut repellat enim voluptatem eos facilis saepe animi voluptate
-        temporibus sit, aut sequi doloribus est minima. Numquam quo itaque
-        accusantium sapiente odit, veritatis incidunt eius earum totam
-        aspernatur, architecto possimus dolorem aut, obcaecati adipisci
-        perferendis soluta saepe consequatur inventore neque quod blanditiis
-        dolor debitis? Esse nostrum quasi atque voluptates at repellendus
-        aliquid dicta, natus repellat, neque quisquam cum quas eligendi impedit
-        voluptatibus ipsa dignissimos incidunt reprehenderit! Illum nostrum aut
-        rerum enim officia. Iste laboriosam impedit iure tenetur cumque nisi
-        obcaecati doloribus!
-      </p>
-    </main>
+    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Hello, E. I. Morrow Co., Inc.!
+        </h1>
+        <p className="max-2-[42rem] leading-normal text-foreground/80 sm:text-xl sm:leading-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla fuga,
+          deserunt obcaecati ab sunt voluptas culpa necessitatibus consequatur
+          ipsa ut repellat enim voluptatem eos facilis saepe animi voluptate
+          temporibus sit, aut sequi doloribus est minima. Numquam quo itaque
+          accusantium sapiente odit, veritatis incidunt eius earum totam
+          aspernatur, architecto possimus dolorem aut, obcaecati adipisci
+          perferendis soluta saepe consequatur inventore neque quod blanditiis
+          dolor debitis? Esse nostrum quasi atque voluptates at repellendus
+          aliquid dicta, natus repellat, neque quisquam cum quas eligendi
+          impedit voluptatibus ipsa dignissimos incidunt reprehenderit! Illum
+          nostrum aut rerum enim officia. Iste laboriosam impedit iure tenetur
+          cumque nisi obcaecati doloribus!
+        </p>
+      </div>
+    </section>
   )
 }
